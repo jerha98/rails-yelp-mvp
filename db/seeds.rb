afd0101cb67556dca86b433820e1a01d8e212e61
Jeremy Hanhoff <jhanhoff@outlook.de>
2024-05-15 16:26:17 +0200

    Back-End Set-Up finished - moving on to Front-End

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts"Cleaning Database..."
+Restaurant.destroy_all
+
+soul = {name: "Soul", address: "Berlin", phone_number: "0123", category: "chinese"}
+honour = {name: "Honour", address: "Paris", phone_number: "4567", category: "french"}
+cheesy = {name: "Cheesy", address: "Rome", phone_number: "8910", category: "italian"}
+fries_b4_guys = {name: "Fries b4 Guys", address: "Antwerp", phone_number: "1112", category: "belgian"}
+fishy = {name: "Fishy", address: "Tokio", phone_number: "1314", category: "japanese"}
+
+[soul, honour, cheesy, fries_b4_guys, fishy].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts"Finished"
